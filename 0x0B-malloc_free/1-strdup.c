6e99cf145c904810c329b888212388da0fafa514
mohiden <mohidenjama@gmail.com>
2023-04-10 21:47:24 -0700

    0x0B task 1 fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,14 +17,17 @@ char *_strdup(char *str)
 	i = 0;
 	len = 0;
 
-	while (str[len])
-		len++;
-
 
 	if (str == NULL)
 		return (NULL);
 
+	while (str[len])
+		len++;
+
 	string = malloc(sizeof(char) * (len + 1));
+
+	if (string == NULL)
+		return (NULL);
 
 	while ((string[i] = str[i]) != '\0')
 		i++;
