5254386ed6362c0d854ea0708b90212a66bff8bb
mohiden <mohidenjama@gmail.com>
2023-04-10 21:13:48 -0700

    0x0B task 0 fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,17 +12,22 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char *array;
+
 	if (size == 0)
 		return (NULL);
 
-	char *array = malloc(size);
+	array = malloc(size);
 
 	if (array == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
+
+	while (i++ < size)
 	{
 		array[i] = c;
 	}
+
 	return (array);
 }
