e1c04dd0d348af40b2b69a3047e26a849d30219b
mohiden <mohidenjama@gmail.com>
2023-04-13 04:58:55 -0700

    0x0C task 1 fix 2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 
-	array = malloc(sizeof(char) * len1 + n + 1);
+	array = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (array == NULL)
 		return (NULL);
