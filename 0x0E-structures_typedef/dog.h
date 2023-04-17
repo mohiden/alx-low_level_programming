d816c6c3270cccefda4eaafa93e8a06d55c81085
mohiden <mohidenjama@gmail.com>
2023-04-17 04:11:04 -0700

    0x0E task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+
+
+/**
+ * struct dog - define new struct
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
