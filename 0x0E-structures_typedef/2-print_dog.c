3103a985ed55c7abcdb8f358ded0093e036a395d
mohiden <mohidenjama@gmail.com>
2023-04-18 01:25:08 -0700

    0x0E task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints dog values
+ * @d: struct
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
