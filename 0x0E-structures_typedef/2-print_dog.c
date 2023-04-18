1d3b392f30beb14c9c2909d90aeba8f064c86f1a
mohiden <mohidenjama@gmail.com>
2023-04-18 02:13:00 -0700

    0x0E task 2 fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,8 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		d->name = "NULL";
+	if (d->owner == NULL)
+		d->owner = "NULL";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
