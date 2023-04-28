ff2299cdb54d03ff48da55f883ca48f6376745aa
mohiden <mohidenjama@gmail.com>
2023-04-28 15:50:51 -0700

    0x12 task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to list_t
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+
+	while (current)
+	{
+		list_t *next = current->next;
+
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
