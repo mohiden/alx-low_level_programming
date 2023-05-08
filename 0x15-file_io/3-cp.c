292ada6f96a55506465d4d25b91e1e1f84362bfa
mohiden <mohidenjama@gmail.com>
2023-05-08 02:29:00 -0700

    0x15 task 3 fix 2

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 	if (fd_from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	fd_to = open(argv[2], O_CREAT |O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (fd_to == -1)
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
