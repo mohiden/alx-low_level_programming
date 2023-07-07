b903978b06c720e37051c95f9127c23ab57bca21
mohiden <mohidenjama@gmail.com>
2023-07-07 13:44:41 -0700

    0x14 adv

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* get_endianness - check endianness
+*
+* Return: 0 if big endian, or 1 if little indian
+*/
+
+int get_endianness(void)
+{
+	int num;
+
+	num = 1;
+
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
