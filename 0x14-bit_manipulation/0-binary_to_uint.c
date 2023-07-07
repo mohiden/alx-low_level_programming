d4c8cdd341a45826c84d3bb6eb7be1ace61bd6b4
mohiden <mohidenjama@gmail.com>
2023-07-06 18:44:32 -0700

    0x14 task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - converts binary to unsigned int
+ * @b: char
+ *
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_unit(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+	int len;
+
+	if (!b)
+		return (0);
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0' || b[i] != '1')
+			result = result * 2 + (b[i] - '0');
+		else
+			return (0);
+	}
+	return (result);
+
+}
