4ffc40394d83dfce7f957333f87ff1fc56797290
mohiden <mohidenjama@gmail.com>
2023-07-10 03:47:32 -0700

    0x15 task0,1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+* read_textfile - reads a txt file
+* @filename: string
+* @letters: size_t
+*
+* Return:  ssize_t
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(0);
+	return (w);
+}
+
